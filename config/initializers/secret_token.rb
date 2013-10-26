6f159b3a8a98e09c0ba31227ebeaad882b9b702b
Vofo <momopatrick90@gmail.com>
2013-10-26 15:16:19 -0400

    Initialising the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learning::Application.config.secret_key_base = 'ad8cbf934135fee6603fa8fe251e9a04f9d70e42eb94464476d364d9bc6914ac70c77c0ae0c754b8df99a82271efd2f107dfa11b558c905b40bee02ea3de5cbe'
